70556ef92e39bb82f7404837c44efc8cc1a7eb4a
wellingtonsouz <>
2026-05-05 13:33:20 +0000

    String Indexing

--- variables_and_types/string_indexing/main.py
+++ variables_and_types/string_indexing/main.py
@@ -6,8 +6,8 @@ first_char = grocery_item[0]
 second_char = grocery_item[8]
 third_char = grocery_item[16]
 
-last_char1 = grocery_item[-17]
-last_char2 = grocery_item[-13]
+last_char1 = grocery_item[-15]
+last_char2 = grocery_item[-7]
 last_char3 = grocery_item[-1]
           
 print("Length of item name:", length_of_item)
